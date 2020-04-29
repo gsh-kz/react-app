394d3dd146e74640f0653e96b1776bc028253bd3
kazu <kazu78@gmail.com>
2020-04-30 03:07:26 +0900

    update

--- readme.md
+++ readme.md
@@ -1,3 +1,54 @@
 # react-app
 
-template
+## Reference
+
+[TypeScript Deep Dive 日本語版 > Browser QuickStart](https://typescript-jp.gitbook.io/deep-dive/browser)
+
+## Setup
+
+```
+$ docker-compose run --rm app npm install
+```
+
+## Development & Run
+
+```
+# dev server start (container start)
+$ docker-compose up -d
+```
+```
+# devserver stop (container down)
+$ docker-compose down
+```
+
+## module build
+
+```
+$ docker-compose exec app npm run build
+```
+or
+```
+$ docker-compose run --rm app npm run build
+```
+
+## Recipe
+
+* react
+* [react-bootstrap](https://react-bootstrap.github.io/)
+* typescript
+* sass
+* webpack
+
+```
+$ npm init -y
+$ npm install --save-exact react react-dom react-router-dom
+$ npm install --save-exact react-bootstrap bootstrap
+$ npm install --save-dev  @types/react @types/react-dom @types/react-router-dom \
+                          @types/react-bootstrap @types/bootstrap \
+                          @types/node
+$ npm install --save-dev typescript ts-loader
+$ npm install --save-dev sass-loader sass css-loader \
+                         mini-css-extract-plugin postcss-loader autoprefixer
+$ npm install --save-dev webpack webpack-cli webpack-dev-server \
+                         clean-webpack-plugin html-webpack-plugin
+```
---
+++ webpack.config.js
@@ -0,0 +1,76 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/js/app.tsx',
+  output: {
+    path: `${__dirname}/dist`,
+    filename: 'js/app.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/templates/index.html'
+    }),
+    new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns: ['dist/*']
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'css/app.css'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader'
+      },
+      {
+        test: /\.(sc|c|sa)ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              url: false,
+              sourceMap: true,
+              importLoaders: 2
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true,
+              plugins: [
+                require('autoprefixer')({
+                  grid: 'autoplace'
+                })
+              ]
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.json']
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    host: '0.0.0.0',
+    port: 8080,
+    hot: true,
+    watchContentBase: true
+  },
+}
